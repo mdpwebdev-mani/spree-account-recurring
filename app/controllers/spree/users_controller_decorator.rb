5317016774db2f3bf50ce45a1c0855e3776604a4
Ricardo Mazzini <mazzini@bit32.it>
2020-02-04 16:03:54 -0500

    Delete users_controller_decorator.rb

--- app/controllers/spree/users_controller_decorator.rb
+++
@@ -1,9 +0,0 @@
-Spree::UsersController.class_eval do
-  before_action :load_subscription_plan, only: :show
-
-  private
-
-    def load_subscription_plan
-      @subscription_plan = spree_current_user.subscription_plans.active.first
-    end
-end
